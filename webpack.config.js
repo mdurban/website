23dd757e26c29bc0f02ccb32689100a68c297955
mdurban <murban117@gmail.com>
2018-09-11 12:46:37 -0400

    add sass loaders

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,18 @@ module.exports = {
           'babel-loader',
         ],
       },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
     ],
   },
   resolve: {
