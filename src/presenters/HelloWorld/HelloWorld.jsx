1e75604b63dbbb1c5106207da2e7a682c3fbc5dc
mdurban <murban117@gmail.com>
2018-09-11 22:13:20 -0400

    initial styling for header

--- src/presenters/HelloWorld/HelloWorld.jsx
+++ src/presenters/HelloWorld/HelloWorld.jsx
@@ -4,7 +4,14 @@ import './hello-world.css'
  
 const HelloWorld = ({ buttonAction }) => (
   <div>
-    <div className='navbar'> Hello</div>
+    <div className='title'>Mike D. Urban</div>
+    <div className='navbar-container'>
+      <div className='navbar-element'><span className='inner-navbar-element'>About</span></div>
+      <div className='navbar-element'><span className='inner-navbar-element'>Resume</span></div>
+      <div className='navbar-element'><span className='inner-navbar-element'>Projects</span></div>
+      <div className='navbar-element'><span className='inner-navbar-element'>Contact</span></div>
+    </div>
+
     <button onClick={ buttonAction }>Action</button>
     <ChildComponentConnector />
   </div>
