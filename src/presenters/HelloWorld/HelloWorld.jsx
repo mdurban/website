2017d6bf63b53befe222b1259b640a7eaf36f1ff
mdurban <murban117@gmail.com>
2018-09-13 22:42:16 -0400

    get initial styling for resume section

--- src/presenters/HelloWorld/HelloWorld.jsx
+++ src/presenters/HelloWorld/HelloWorld.jsx
@@ -5,17 +5,58 @@ import './hello-world.css'
 const HelloWorld = ({ buttonAction }) => (
   <div>
     <div className='title'>Michael Urban</div>
-    <hr />
     <div className='navbar-container'>
       <div className='navbar-element'><span className='inner-navbar-element'>about</span></div>
       <div className='navbar-element'><span className='inner-navbar-element'>resume</span></div>
       <div className='navbar-element'><span className='inner-navbar-element'>projects</span></div>
       <div className='navbar-element'><span className='inner-navbar-element'>contact</span></div>
     </div>
+    <hr />
 
+  {/*<div className='about'>
+    <div className='header'>about</div>
+    <div className='content'>
+      I want to make robots that help people instead of creating robots that take away people's jobs.
+    </div>
+  </div>*/}
+  <div className='header'></div>
+  <div className='resume-container'>
+    <div className='company-names'>
+      <div className='company'>Pillar Technology</div>
+      <div className='company-info'>
+        <div className='date'>2014 - Present</div>
+        <div className='position'>Senior Software Engineer</div>
+      </div>
+      <div className='company'>Cisco Systems</div>
+      <div className='company-info'>
+        <div className='date'>2012 - 2014</div>
+        <div className='position'>Software Engineer</div>
+      </div>
+    </div>
+    <div className='job-descriptions'>
+      <ul>
+      <li>
+        Android infotainment system that provides drivers with phone, audio and navigation suggestions based off learning algorithms
+      </li>
+      <li>
+        Web application which helps patients schedule services with their healthcare providers
+      </li>
+      <li>
+        Mobile application to help users choose a doctor based on needs and reviews
+      </li>
+      <li>
+        Warehouse execution system used to help company ship merchandise out of distribution center
+      </li>
+      </ul>
+    </div>
+  </div>
+  <div className='projects'></div>
+  <div className='contact'></div>
 
+  {/*
     <button onClick={ buttonAction }>Action</button>
     <ChildComponentConnector />
+  */}
   </div>
 )
 
